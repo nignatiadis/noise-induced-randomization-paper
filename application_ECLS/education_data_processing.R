23a25d0cfdd677fc57cd0446f07c2635fa7bf80c
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2022-04-11 00:45:19 -0700

    edits to data processing file

--- application_ECLS/education_data_processing.R
+++ application_ECLS/education_data_processing.R
@@ -1,25 +1,25 @@
 library(readstata13)
 library(REBayes)
 
-setwd("~/Desktop/stanford/Autumn 2019/Stefan/code")
 # get data 
-data = read.dta13("data/ECLS_data.dta")
+data <- read.dta13("ECLS_data.dta")
+
 ##cleaning up NA's 
-get_data = function(base_score, base_std, year1_score, year2_score) {
-  idx = !is.na(year1_score) & !is.na(year2_score) & !is.na(base_std) & base_std > 0 & !is.na(base_score) &
+get_data <- function(base_score, base_std, year1_score, year2_score) {
+  idx <- !is.na(year1_score) & !is.na(year2_score) & !is.na(base_std) & base_std > 0 & !is.na(base_score) &
     (base_score >= -8) & (base_score <= 8) & 
     (year1_score >= -8) & (year1_score <= 8) & 
     (year2_score >= -8) & (year2_score <= 8)
-  result = data.frame(base_score = base_score[idx],
+  result <- data.frame(base_score = base_score[idx],
                       base_std = base_std[idx],
                       year1_score = as.numeric(year1_score[idx] > 0.5), #dichotomization at 0.5
                       year2_score = as.numeric(year2_score[idx] > 0.5))
   return(result)
 }
 
-# get data we actually see
-get_outcome = function(d, cutoff) {
-  idx = (d$base_score > cutoff)
+# generate observed outcome
+get_outcome <- function(d, cutoff) {
+  idx = (d$base_score >= cutoff)
   Y = numeric(length(d$base_score))
   Y[idx] = d$year2_score[idx]
   Y[!idx] = d$year1_score[!idx]
@@ -29,49 +29,61 @@ get_outcome = function(d, cutoff) {
   return(result)
 }
 
+# Construct artificial RDD 
 
-true_d = get_data(data$X2MTHETK5, data$X2MSETHK5, data$X4MTHETK5, data$X6MTHETK5)
-c = -0.2
-d = get_outcome(true_d, c)
-z = d$z
-std = d$std
-Y = d$Y
-data = data.frame(z= z, Y = Y, std = std)
+true_d <- get_data(data$X2MTHETK5, data$X2MSETHK5, data$X4MTHETK5, data$X6MTHETK5)
+c <- -0.2
+d <- get_outcome(true_d, c)
+
+# Save artificial RDD dataset
+
+write.csv(d, "ecls_RD.csv", row.names=FALSE)
 
 
-# getting ground truth 
-# the ground truth for E[Y(1) - Y(0)|Z]
-get_true_tau = function(u) {
-  fit = smooth.spline(true_d$base_score, true_d$year2_score - true_d$year1_score)
+
+# Compute "ground truth" for E[Y(1) - Y(0)|Z]
+get_true_tau <- function(u) {
+  fit <- smooth.spline(true_d$base_score, true_d$year2_score - true_d$year1_score)
   return(predict(fit, u)$y)
 }
 
-## getting policy parmeter for C' where C' varies 
-c = -0.2
-d = get_outcome(true_d, c)
-z = d$z
-Y = d$Y
-std = d$std
-variance = min(std)^2
-my_seq = seq(from = -0.2, to = 0.2, by = 0.02)
-my_seq = my_seq[my_seq != 0]
+cs <- seq(from = -0.4, to = 0.0, by = 0.02)
 
-true_seq = numeric(length(my_seq))
-est = GLmix(z, hist = TRUE, sigma = sqrt(variance))
+rdd_target_pseudo_ground_truth <- sapply(cs, get_true_tau)
+
+write.csv(data.frame(basescore=cs, truth=rdd_target_ground_truth), 
+      "rdd_target_pseudo_ground_truth.csv", row.names=FALSE)
+
+
+## Compute "ground truth" policy parameter for C' where C' varies 
+
+my_seq <- seq(from = -0.2, to = 0.2, by = 0.02)
+my_seq <- my_seq[my_seq != 0]
+z <- d$z
+Y <- d$Y
+std <- d$std
+variance <- min(std)^2
+
+
+policy_param_pseudo_ground_truth <- numeric(length(my_seq))
+est_glmix <- GLmix(z, hist = TRUE, sigma = sqrt(variance))
+
 # given the observed z, return an estimated density at a vector of values
-z_dens = function(est, vec) {
-  mat = as.matrix(sapply(vec, dnorm, mean = est$x, sd = est$sigma))
-  result = est$y%*%mat
+z_dens <- function(est, vec) {
+  mat <- as.matrix(sapply(vec, dnorm, mean = est$x, sd = est$sigma))
+  result <- est$y %*% mat
   return(result)
 }
 
 for (i in 1:length(my_seq)){
   if (my_seq[i] >= 0) {
-    true_seq[i] = integrate(function(z) get_true_tau(z)*z_dens(est, z), c, c+my_seq[i])$value/integrate(function(z) z_dens(est, z), c, c+my_seq[i])$value
+    policy_param_pseudo_ground_truth[i] <- integrate(function(z) get_true_tau(z)*z_dens(est_glmix, z), c, c+my_seq[i])$value/integrate(function(z) z_dens(est_glmix, z), c, c+my_seq[i])$value
   }
   else{
-    true_seq[i] = integrate(function(z) get_true_tau(z)*z_dens(est, z), c+my_seq[i], c)$value/integrate(function(z) z_dens(est, z), c+my_seq[i], c)$value
+    policy_param_pseudo_ground_truth[i] <- integrate(function(z) get_true_tau(z)*z_dens(est_glmix, z), c+my_seq[i], c)$value/integrate(function(z) z_dens(est_glmix, z), c+my_seq[i], c)$value
   }
 }
 
-policy_truth = data.frame(c_prime = c + my_seq, policy_par = true_seq)
+policy_truth <- data.frame(c_prime = c + my_seq, policy_par = policy_param_pseudo_ground_truth)
+write.csv(policy_truth,
+      "policy_target_pseudo_ground_truth.csv", row.names=FALSE)
